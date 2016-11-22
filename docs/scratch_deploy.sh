0b39e19183978a356c83768527814abf861bb239
dougbtv <douglaskippsmith@gmail.com>
2016-11-21 19:28:18 -0500

    [vars][docs] updates instack to limit some nodes for a minute, updates scratch deploy

--- docs/scratch_deploy.sh
+++ docs/scratch_deploy.sh
@@ -1,3 +1,42 @@
+# -------------- for some damn reason when I would do both compute and control at once, there'd be a shitty situation where controller wouldn't 
+# have the right default route.
+# so I'm busting it into two moves for the short time being
+
+openstack overcloud deploy \
+   --control-flavor control --compute-flavor compute --ceph-storage-flavor ceph-storage \
+   --templates ~/my_templates \
+   -e ~/my_templates/network-environment.yaml \
+   -e ~/my_templates/environments/network-isolation.yaml \
+   --compute-scale 0 \
+   --control-scale 1 && \
+openstack overcloud deploy \
+   --control-flavor control --compute-flavor compute --ceph-storage-flavor ceph-storage \
+   --templates ~/my_templates \
+   -e ~/my_templates/network-environment.yaml \
+   -e ~/my_templates/environments/network-isolation.yaml \
+   --compute-scale 1 \
+   --control-scale 1
+
+# ----------------------------------- add in the compute node
+
+openstack overcloud deploy \
+   --control-flavor control --compute-flavor compute --ceph-storage-flavor ceph-storage \
+   --templates ~/my_templates \
+   -e ~/my_templates/network-environment.yaml \
+   -e ~/my_templates/environments/network-isolation.yaml \
+   --compute-scale 1 \
+   --control-scale 1
+
+# ---------------------------------- this is the one that worked.
+
+openstack overcloud deploy \
+   --control-flavor control --compute-flavor compute --ceph-storage-flavor ceph-storage \
+   --templates ~/my_templates \
+   -e ~/my_templates/network-environment.yaml \
+   -e ~/my_templates/environments/network-isolation.yaml \
+   --compute-scale 0 \
+   --control-scale 1
+
 
 [stack@undercloud ~]$ openstack overcloud deploy   --control-flavor control --compute-flavor compute --ceph-storage-flavor ceph-storage   --templates ~/my_templates   -e ~/my_templates/network-environment.yaml   -e ~/my_templates/environments/network-isolation.yaml   --compute-scale 0   --control-scale 1
 
